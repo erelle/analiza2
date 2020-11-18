7ff73c2066bfda5c6cd26207e1d6a15e01884204
erelle <reina.boubli1998@gmail.com>
2020-11-18 18:44:12 +0200

    finding LUD and D-1

--- main.py
+++ main.py
@@ -1,16 +1,116 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+matrix=[]
+row=[]
+rows= int(input("enter number of rows: "))
+for i in range (rows):
+    for i in range (rows):
+        num=float(input("enter a number "))
+        row.append(num)
+    matrix.append(row)
+    row = []
+print("your matrix is " ,matrix)
+solution=[]
+for i in range (rows):
+    num = float(input("enter a number for solution"))
+    solution.append(num)
+
+def dominant(matrix):
+    sumNum=0
+    flag=0
+    for i in range(len(matrix)):
+        sumNum=abs(sum(matrix[i]))-abs(matrix[i][i])
+        if abs(matrix[i][i])<sumNum:
+            flag+=1
+    return flag
+
+def EmptyMatrix(matrix):
+    m=[]
+    row=[]
+    num = 0
+    for i in range(len(matrix)):
+        for i in range(len(matrix)):
+            row.append(num)
+        m.append(row)
+        row = []
+    return m
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def findLUD(matrix):
+    x0 = 0
+    L = EmptyMatrix(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            int(i)
+            int(j)
+            if j >= i:
+                L[i][j] = 0
+            else:
+                L[i][j] = int(matrix[i][j])
+    print("L :", L)
+    U = EmptyMatrix(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            int(i)
+            int(j)
+            if j <= i:
+                U[i][j] = 0
+            else:
+                U[i][j] = int(matrix[i][j])
+    print("U :", U)
+    D = EmptyMatrix(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            int(i)
+            int(j)
+            if j == i:
+                D[i][j] = int(matrix[i][j])
+            else:
+                D[i][j] = 0
+
+    print("D :", D)
+    return L,U,D
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def yahakobi(matrix,solution):
+    dom = dominant(matrix)
+    if dom > 0:
+        print("non dominant matrix ")
+        return
+    else:
+        print("dominant matrix ")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+    L,U,D=findLUD(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+          if D[i][j]!=0:
+              D[i][j]*=1/10
+
+
+
+
+
+
+
+
+def zaidel(matrix, solution):
+    dom = dominant(matrix)
+    if dom > 0:
+        print("non dominant matrix ")
+        return
+    else:
+        print("dominant matrix ")
+    findLUD(matrix)
+
+
+
+choice=int(input("which one do you want to use: 1 yahakobi, 2 zaidel "))
+if choice==1:
+    yahakobi(matrix,solution)
+if choice==2:
+    zaidel(matrix,solution)
+
+
+
